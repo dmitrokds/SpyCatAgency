44f29dc663df8e2654e0b373630b0c310eadba66
dmitrokds <dmitrokds@gmail.com>
2026-01-05 11:11:26 +0200

    main_update

--- main.py
+++ main.py
@@ -2,11 +2,16 @@ from fastapi import FastAPI
 import asyncio
 import uvicorn
 
+from routers.spy_cats import router as spy_cats_router
+
 
 async def main():
     app = FastAPI()
     
+    app.include_router(spy_cats_router, prefix="/spy-cat", tags=["Spy Cats"])
+    
     uvicorn.run(app=app)
+    
     
 if __name__ == "__main__":
     asyncio.run(main())
